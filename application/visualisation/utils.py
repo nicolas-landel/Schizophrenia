2254b61f665095abc2afe3e744c2c68a1358e012
Nicolas Landel <nicolas.landel@hotmail.com>
2020-11-18 14:45:24 +0100

    Import the function interactive_plot_variable_by_variable

--- application/visualisation/utils.py
+++ application/visualisation/utils.py
@@ -1,4 +1,10 @@
+import math
+
 import plotly.graph_objects as go
+
+# todo table is table_explained_mca
+from plotly.offline import offline
+
 
 def prepare_square_plotly(fs_id1, fs_id2, table):
     """ This function plots a square. It allows us to determine the modalities where the contribution ((weight*(aj)**2))/eigenvalue
@@ -29,4 +35,97 @@ def prepare_square_plotly(fs_id1, fs_id2, table):
                            mode='lines',
                            line=go.scatter.Line(color="orange"))
 
-    return ([top_line, bottom_line, right_line, left_line])
+    return [top_line, bottom_line, right_line, left_line]
+
+
+def interactive_plot_variable_by_variable(table, table_explained_mca, fs_id1, fs_id2, display, square=False,
+                                          significant_only=False):
+    """
+    Categories of the disjunctive data frame in the factor plan. The axis can be modified to chose the factor you want (integer
+    between 1 and 10)
+    Each variable is represented as a point (using scatter) and they can be removed of the graph in order to focus just on some
+    variables
+
+    significant_only is a bool, if false, display all the modalities, if true, only those outside the square (contribution>weight)
+
+    The variables are represented in blue, the label is in red
+
+    Display (bool) : if true, the graph is directly plot, if False, this returns data_ and layout (usefull for the dash)
+    The graph is automatically saved as html in the repository current_repository/Images
+
+    """
+    # todo table is table_modalities_mca: see what impact
+    fs = 'Factor'
+
+    list_name_col_plot = [str(table.columns[i][0]) + ' | ' + str(table.columns[i][1]) for i in
+                          range(len(table.columns))]
+
+    data_col = []
+    data_lab = []
+    coordinates_max = max(max(abs(table.loc[(fs, fs_id1)].values)), max(abs(table.loc[(fs, fs_id2)].values)))
+
+    for i in range(table.shape[1] - 3):
+        if significant_only:  # if we plot only the significant modalities ie outside the saquare
+            if abs(table.iloc[fs_id1 - 1, i]) > math.sqrt(table_explained_mca.loc[fs_id1, 'Zλ']) and \
+                    abs(table.iloc[fs_id2 - 1, i]) > math.sqrt(table_explained_mca.loc[fs_id2, 'Zλ']):
+                cols_table = go.Scatter(x=[table.iloc[fs_id1 - 1, i]],
+                                        y=[table.iloc[fs_id2 - 1, i]],
+                                        hovertext=[list_name_col_plot[i]],
+                                        mode='markers',
+                                        name='variable {}'.format(list_name_col_plot[i]),
+                                        marker=dict(size=10, color="rgba(0,0,255,0.7)"))
+                data_col.append(cols_table)
+
+        elif significant_only == False:
+            cols_table = go.Scatter(x=[table.iloc[fs_id1 - 1, i]],
+                                    y=[table.iloc[fs_id2 - 1, i]],
+                                    hovertext=[list_name_col_plot[i]],
+                                    mode='markers',
+                                    name='variable {}'.format(list_name_col_plot[i]),
+                                    marker=dict(size=10, color="rgba(0,0,255,0.7)"))
+
+            data_col.append(cols_table)
+
+    for j in range(1, 4):  # the last columns of the table are label ('aps de risque', 'a risque', 'psychose')
+        if j == 1:  # psychose
+            color_label = "rgba(255,0,0,0.7)"
+        elif j == 2:  # a risque
+            color_label = 'rgba(200,180,40,0.9)'
+        elif j == 3:  # pas de risque
+            color_label = 'rgba(0,220,0,0.7)'
+        label_table = go.Scatter(x=[table.iloc[fs_id1 - 1, -j]],
+                                 y=[table.iloc[fs_id2 - 1, -j]],
+                                 mode='markers',
+                                 hovertext=[list_name_col_plot[-j]],
+                                 name='{}'.format(list_name_col_plot[-j]),
+                                 marker=dict(size=10, color=color_label))
+
+        data_lab.append(label_table)
+
+    data_ = data_col + data_lab
+
+    if square == True:
+        data = data_ + prepare_square_plotly(fs_id1, fs_id2)  # if the option 'square limit' is active
+        # it will plot a square
+    else:
+        data = data_
+
+    layout = go.Layout(title="Coordonnées des modalités selon les facteurs " + str(fs_id1) + ' et ' + str(fs_id2),
+                       xaxis={"title": "factor" + str(fs_id1),
+                              "range": [-coordinates_max - 0.1, coordinates_max + 0.1]},
+                       # "gridcolor":"rgba(150,150,150,0.7)", "color":"rgba(100,100,100,0.7)"},
+                       yaxis={"title": "factor" + str(fs_id2), "range": [-coordinates_max - 0.1, coordinates_max + 0.1]}
+                       # "gridcolor":"rgba(150,150,150,0.7)", "color":"rgba(100,100,100,0.7)"}
+                       )
+
+    fig = go.Figure(data=data, layout=layout)
+
+    offline.plot(fig, filename='Images/Variables dans le plan des facteurs scores.html',
+                 # to save the figure in the repertory
+                 auto_open=False)
+
+    if display:
+        offline.iplot(fig)
+        return None
+    else:
+        return data, layout
