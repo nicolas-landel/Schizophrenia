cc15cc758f3a5af13b5a45f4c08b8b9094ba896f
Nicolas LAndel <nicolas.landel@hotmail.com>
2020-12-29 18:46:07 +0100

    Rename html class instance

--- application/app.py
+++ application/app.py
@@ -1,10 +1,11 @@
 import dash
 import dash_html_components as html
 
-from template.html_app import GenerateApp
+from template import GenerateApp
 
 app = dash.Dash(__name__)
-app.layout = html.Div(children=GenerateApp().html_list)
+
+app.layout = html.Div(children=GenerateApp().html)
 
 if __name__ == '__main__':
     app.run_server(host='0.0.0.0', port=8000, debug=True)
