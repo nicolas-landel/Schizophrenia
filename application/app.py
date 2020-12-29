cb92eb3f85a43cce9dd760e355e55d8bb0104d1e
Nicolas LAndel <nicolas.landel@hotmail.com>
2020-12-29 16:46:17 +0100

    Order the app.py file

--- application/app.py
+++ application/app.py
@@ -1,11 +1,10 @@
 import dash
 import dash_html_components as html
 
-from flask import Flask
+from template.html_app import GenerateApp
 
-server = Flask(__name__)
-app = dash.Dash(server=server)
-app.layout = html.Div("Hello world.")
+app = dash.Dash(__name__)
+app.layout = html.Div(children=GenerateApp().html_list)
 
 if __name__ == '__main__':
-    app.run_server()
+    app.run_server(host='0.0.0.0', port=8000, debug=True)
